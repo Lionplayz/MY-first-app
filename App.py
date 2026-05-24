cd2f1a10d97766990d9eb18db2d5b8cc141198a3
Lionplayz <lionplayz098@gmail.com>
2026-05-24 20:09:48 +0545

    just update ballons

--- App.py
+++ App.py
@@ -124,7 +124,7 @@ if "current_line" not in st.session_state:
 
 if st.button("Generate Flirt Line 💌"):
     st.session_state.current_line = random.choice(flirt_lines)
-    st.balloons()
+    st.hearts()
 
 if st.session_state.current_line:
     st.markdown(f'<div class="line-box">{st.session_state.current_line}</div>', unsafe_allow_html=True)
